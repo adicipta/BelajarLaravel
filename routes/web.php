664f43994ccf41165a52b03457143150b40342a7
Adi Cipta Pratama <only.adicipta@gmail.com>
2022-03-22 08:45:25 +0700

    use blade templating engine

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,69 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home',[
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "About",
+        "name" => "Adi Cipta Pratama",
+        "email" => "only.adicipta@gmail.com",
+        "image" => "adi.jpg",
+    ]);
+});
+
+
+Route::get('/blog', function () {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "jusul-post-pertama",
+            "author" => "Adi Cipta Pratama",
+            "body" => "Ini adalah Post Pertama Adi Cipta Pratama. Dibuat di tanggal 21 Maret 2022 Oleh Adi Cipta Pratama",
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "jusul-post-kedua",
+            "author" => "Liestiani Hasan",
+            "body" => "Ini adalah Post Kedua Liestiani Hasan. Dibuat di tanggal 21 Maret 2022 Oleh Liestiani Hasan.",
+        ],
+    ];
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+// halaman single post
+Route::get('posts/{slug}', function($slug){
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "jusul-post-pertama",
+            "author" => "Adi Cipta Pratama",
+            "body" => "Ini adalah Post Pertama Adi Cipta Pratama. Dibuat di tanggal 21 Maret 2022 Oleh Adi Cipta Pratama",
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "jusul-post-kedua",
+            "author" => "Liestiani Hasan",
+            "body" => "Ini adalah Post Kedua Liestiani Hasan. Dibuat di tanggal 21 Maret 2022 Oleh Liestiani Hasan.",
+        ],
+    ];
+
+
+    $new_post = [];
+    foreach($blog_posts as $post) {
+        if($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
