cf5cc0e296fc0544a730da986208bb3e2ca4f109
Adi Cipta Pratama <only.adicipta@gmail.com>
2022-03-23 10:43:26 +0700

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +19,53 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Adi',
+            'email' => 'only.adicipta@gmail.com',
+            'password' => bcrypt('adisty2026')
+        ]);
+
+        User::create([
+            'name' => 'Cipta',
+            'email' => 'ciptapratama@gmail.com',
+            'password' => bcrypt('adisty12175')
+        ]);
+
+        Category::create([
+            'name' => 'Programmer',
+            'slug' => 'Programmer'
+        ]);
+
+        Category::create([
+            'name' => 'Web Design',
+            'slug' => 'web-design'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Ini adalah tulisan saya sebagai Junior Software Developer,',
+            'body' => 'Ini adalah tulisan saya sebagai Junior Software Developer, saya adalah Adi dengan background S1 Teknik Informatika Universitas Pamulang',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Ini adalah tulisan saya sebagai QA Engineer,',
+            'body' => 'Ini adalah tulisan saya sebagai QA Engineer, saya adalah Cipta dengan background S1 Sistem Informasi Universitas UIN Syarif Hidayatullah',
+            'category_id' => 1,
+            'user_id' => 2
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Tiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Ini adalah tulisan saya sebagai Web Designer,',
+            'body' => 'Ini adalah tulisan saya sebagai Web Designer, saya adalah Pratama dengan background S1 Desain Grafis Universitas Gunadarma',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
     }
 }
