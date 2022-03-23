63fa61425572208e2f297a82fbacdff11ed5b501
Adi Cipta Pratama <only.adicipta@gmail.com>
2022-03-23 11:20:11 +0700

    add data user use factory, add factory for post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,18 +18,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        User::create([
-            'name' => 'Adi',
-            'email' => 'only.adicipta@gmail.com',
-            'password' => bcrypt('adisty2026')
-        ]);
 
-        User::create([
-            'name' => 'Cipta',
-            'email' => 'ciptapratama@gmail.com',
-            'password' => bcrypt('adisty12175')
-        ]);
+        // User::create([
+        //     'name' => 'Adi',
+        //     'email' => 'only.adicipta@gmail.com',
+        //     'password' => bcrypt('adisty2026')
+        // ]);
+
+        // User::create([
+        //     'name' => 'Cipta',
+        //     'email' => 'ciptapratama@gmail.com',
+        //     'password' => bcrypt('adisty12175')
+        // ]);
+
+        User::factory(5)->create();
 
         Category::create([
             'name' => 'Programmer',
@@ -41,31 +43,38 @@ class DatabaseSeeder extends Seeder
             'slug' => 'web-design'
         ]);
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Ini adalah tulisan saya sebagai Junior Software Developer,',
-            'body' => 'Ini adalah tulisan saya sebagai Junior Software Developer, saya adalah Adi dengan background S1 Teknik Informatika Universitas Pamulang',
-            'category_id' => 1,
-            'user_id' => 1
+        Category::create([
+            'name' => 'QA Engineer',
+            'slug' => 'qa-engineer'
         ]);
 
-        Post::create([
-            'title' => 'Judul Ke Dua',
-            'slug' => 'judul-ke-dua',
-            'excerpt' => 'Ini adalah tulisan saya sebagai QA Engineer,',
-            'body' => 'Ini adalah tulisan saya sebagai QA Engineer, saya adalah Cipta dengan background S1 Sistem Informasi Universitas UIN Syarif Hidayatullah',
-            'category_id' => 1,
-            'user_id' => 2
-        ]);
+        Post::factory(20)->create();
 
-        Post::create([
-            'title' => 'Judul Ke Tiga',
-            'slug' => 'judul-ke-tiga',
-            'excerpt' => 'Ini adalah tulisan saya sebagai Web Designer,',
-            'body' => 'Ini adalah tulisan saya sebagai Web Designer, saya adalah Pratama dengan background S1 Desain Grafis Universitas Gunadarma',
-            'category_id' => 2,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Ini adalah tulisan saya sebagai Junior Software Developer,',
+        //     'body' => 'Ini adalah tulisan saya sebagai Junior Software Developer, saya adalah Adi dengan background S1 Teknik Informatika Universitas Pamulang',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Ke Dua',
+        //     'slug' => 'judul-ke-dua',
+        //     'excerpt' => 'Ini adalah tulisan saya sebagai QA Engineer,',
+        //     'body' => 'Ini adalah tulisan saya sebagai QA Engineer, saya adalah Cipta dengan background S1 Sistem Informasi Universitas UIN Syarif Hidayatullah',
+        //     'category_id' => 1,
+        //     'user_id' => 2
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Ke Tiga',
+        //     'slug' => 'judul-ke-tiga',
+        //     'excerpt' => 'Ini adalah tulisan saya sebagai Web Designer,',
+        //     'body' => 'Ini adalah tulisan saya sebagai Web Designer, saya adalah Pratama dengan background S1 Desain Grafis Universitas Gunadarma',
+        //     'category_id' => 2,
+        //     'user_id' => 1
+        // ]);
     }
 }
