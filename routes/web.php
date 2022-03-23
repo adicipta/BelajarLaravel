27733ec04f3104cd8d64a59b9b174a5180253c91
Adi Cipta Pratama <only.adicipta@gmail.com>
2022-03-23 09:56:41 +0700

    implement relationship

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
+use App\Models\Category;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -32,4 +34,11 @@ Route::get('/about', function () {
 
 
 Route::get('/post', [PostController::class, 'index']);
-Route::get('posts/{post:slug}', [PostController::class, 'show']);
+Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+Route::get('/categories/{category:slug}', function(Category $category) {
+    return view('category', [
+        'title' => $category->name,
+        'posts' => $category->posts,
+        'category' => $category->name
+    ]);
+});
