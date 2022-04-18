0aacc2ca3e7dd820512bc7bb80b65057ac90e8e2
Adi Cipta Pratama <only.adicipta@gmail.com>
2022-04-18 09:57:38 +0700

    make a card for post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,7 +5,7 @@
 
 @if($posts->count())
 <div class="card mb-3">
-  <img src="https://source.unsplash.com/1200x400/?{{ $posts[0]->category->name }}" class="card-img-top" alt="...">
+  <img src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}">
   <div class="card-body text-center">
     <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}"  class="text-decoration-none text-dark" >{{ $posts[0]->title }}</a></h3>
     <small class="text-muted">
@@ -20,28 +20,22 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-md-4">
+    @foreach ($posts->skip(1) as $post)
+        <div class="col-md-4 mb-3">
             <div class="card">
-                <img src="..." class="card-img-top" alt="...">
+            <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
+                <h5 class="card-title">{{ $post->title }}</h5>
+                <small class="text-muted">
+                <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name  }}</a> {{ $post->created_at->diffForHumans() }}</small></p>
+                    <p class="card-text">{{ $post->excerpt }}</p>
+                    <a href="/posts/{{ $post->slug }}" class="btn btn-primary">Read more...</a>
+                </div> 
             </div>
         </div>
+        @endforeach
     </div>
 </div>
-
-    @foreach ($posts->skip(1) as $post)
-    <article class="mb-5 border-bottom pb-4">
-    <h2><a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a></h2>
-    <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name  }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
-    <p>{{ $post->excerpt }}</p>
-
-    <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more..</a>
-    </article>
-    @endforeach
 
 @endsection
 
