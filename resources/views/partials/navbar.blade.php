b7f34a178705d2bcf3aa067fceee4422207fdb61
Adi Cipta Pratama <only.adicipta@gmail.com>
2022-05-11 14:36:46 +0700

    fix categories pages

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -15,6 +15,9 @@
         <li class="nav-item">
           <a class="nav-link {{ ($title === "Post") ? 'active' : '' }}" href="/post">Blog</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link {{ ($title === "Categories") ? 'active' : '' }}" href="/categories">Categories</a>
+        </li>
       </ul>
     </div>
   </div>
